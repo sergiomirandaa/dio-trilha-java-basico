0bc90b74aec2910d2579d9c09fa3bcde6051542f
sergiomirandaa <smgdev@outlook.com.br>
2025-04-02 23:24:38 -0300

    sistema smart-tv

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,3 +1,35 @@
 public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void diminuirVolumeCanal (){
+        canal--;
+    }
+
+    public void aumentarCanal (){
+        canal++;
+    }
+
+    public void diminuirVolume(){
+        volume--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void ligar (){
+        ligada = true;
+    }
+
+    public void desligar (){
+        ligada = false; 
+    }
     
 }
