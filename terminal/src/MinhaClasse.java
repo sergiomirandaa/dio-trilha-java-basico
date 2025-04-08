9421c471ab52dc24177cd3558ccade45e3f95f24
sergiomirandaa <smgdev@outlook.com.br>
2025-04-07 21:14:15 -0300

    terminal

--- terminal/src/MinhaClasse.java
+++ terminal/src/MinhaClasse.java
@@ -1,6 +1,6 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("novo terminal ");
+        System.out.println("novo terminal java");
     }
 
 }
